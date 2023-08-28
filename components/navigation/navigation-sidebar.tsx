f4c463ef0a8ca9f455d193103a2a4528ad299318
Abhishek Gupta <91587690+iamabhigupta@users.noreply.github.com>
2023-08-28 20:40:45 +0530

    add create server button

---
+++ components/navigation/navigation-sidebar.tsx
@@ -0,0 +1,30 @@
+import { currentProfile } from "@/lib/current-profile"
+import { db } from "@/lib/db"
+import { redirect } from "next/navigation"
+import { NavigationAction } from "./navigation-action"
+
+const NavigationSidebar = async () => {
+  const profile = await currentProfile()
+
+  if (!profile) {
+    return redirect("/")
+  }
+
+  const servers = await db.server.findMany({
+    where: {
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+  })
+
+  return (
+    <div className="flex h-full w-full flex-col items-center space-y-4 bg-[#E3E5E8] py-3 text-primary dark:bg-[#1E1F22]">
+      <NavigationAction />
+    </div>
+  )
+}
+
+export default NavigationSidebar
